4f0f8db0a1830fad69b7eddaab97926d5a83c404
daipengwa <daipengwa@gmail.com>
2025-06-01 17:28:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,17 +16,17 @@
 
 # leftview2rightview
 python create_frame_matrix.py --img_path=./Videos/An_astronaut_in_full_space_suit_riding_a_horse_320x576/ --depth_path=./Videos/An_astronaut_in_full_space_suit_riding_a_horse_320x576/ --output_path=./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320 --max_baseline 0.07 --num_training_views 8  --left2right
-python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40
+python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --erode_size 3
 
 # # rightview2leftview
 # python create_frame_matrix.py --img_path=./Videos/An_astronaut_in_full_space_suit_riding_a_horse_320x576/ --depth_path=./Videos/An_astronaut_in_full_space_suit_riding_a_horse_320x576/ --output_path=./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320 --max_baseline 0.07 --num_training_views 8  --right2left
-# python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --right2left
+# python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --right2left --erode_size 3
 
-# # middle2leftright (best perfromace with a liitle more time)
+# # middle2leftright (best performance)
 # python create_frame_matrix.py --img_path=./Videos/An_astronaut_in_full_space_suit_riding_a_horse_320x576/ --depth_path=./Videos/An_astronaut_in_full_space_suit_riding_a_horse_320x576/ --output_path=./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320 --max_baseline 0.07 --num_training_views 8  --middle2leftright
-# python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --middle2leftright
+# python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --middle2leftright --erode_size 3
 
-# # multi-view generation
-# python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --frame_matrix_end 0 --fix_last_view_begin 600
+# # multi-view video generation
+# python svg.py --prompt 'An astronaut in full space suit riding a horse' --init_image ./Videos/frame_matrix/An_astronaut_in_full_space_suit_riding_a_horse --output_root ./results/An_astronaut_in_full_space_suit_riding_a_horse/ --num_frames 16 --width 576 --height 320  --latent_w 72 --latent_h 40 --frame_matrix_end 0 --fix_last_view_begin 600 --erode_size 3
 
 
